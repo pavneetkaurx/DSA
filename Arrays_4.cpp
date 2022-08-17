26cc199d315e1f9319f9c612fe2f0b667bfc2f7b
Pavneet Kaur <pavneetkaur58@gmail.com>
2022-08-17 16:59:21 +0530

    Add files via upload

---
+++ Arrays_4.cpp
@@ -0,0 +1,16 @@
+//User function Template for C++
+//Print alternate elements of an array
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    for(int i=0;i<n;i=i+2)
+    cout<<ar[i]<<" ";
+    
+    // code here
+    
+    
+}
+
+
